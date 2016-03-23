1a8ba88f812ce87605850ed74d6cd32595b79830
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-03-23 11:08:20 -0700

    added track name

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 // wait for DOM to load before running JS
+
+var spotifyApi = 'https://api.spotify.com/v1/search';
+
 $(document).on('ready', function() {
 
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
+  $('form').on("submit", function (event) {
+    event.preventDefault();
+    getTracks();
+  });
 
-  // your code here
+  function getTracks () {
+    $.ajax({
+      method: 'GET',
+      url: spotifyApi,
+      data: $('form').serialize(),
+      success: onSuccess,
+      error: onError
+    });
+  }
 
+  function onSuccess (dataFromServer) {
+    console.log(dataFromServer);
+    $('#results').html('');
+    dataFromServer.tracks.items.forEach(function (item) {
+      $('#results').append('<img src="' + item.album.images[1].url +'">');
+      $('#results').append('<p>' + item.artists[0].name + '</p>');
+      $('#results').append('<a href="' + item.artists[0].preview_url + '">'); //opens link in a new tab
+
+    });
+  }
+
+  function onError (error) {
+    console.log(error);
+  }
 });
