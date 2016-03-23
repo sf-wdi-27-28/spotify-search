18ea8fd75068abebadef569a9d39ebc70221e0a2
jtam11 <justinktam@gmail.com>
2016-03-22 23:09:30 -0700

    Able to take user search and return artist and track title

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    getInfo();
+  });
   // check to make sure JS is loaded
   console.log('JS is loaded!');
-
-  // your code here
-
+function getInfo() {
+  $.ajax({
+    method: "GET",
+    url: 'https://api.spotify.com/v1/search',
+    data: $('form').serialize(),
+    success:getTrackInfo,
+    error:onError
+  });
+}
 });
+
+
+function getTrackInfo(blarg) {
+  $('.info').remove();
+  blarg.tracks.items.forEach(function(element){
+    $('#results').append('<li class="info">' + element.name + ' by ' + element.artists[0].name + '</li>');
+  });
+}
+
+function onError(xhr, status, errorThrown) {
+  alert("Sorry, there was a problem!");
+  console.log("Error: " + errorThrown);
+  console.log("Status: " + status);
+  console.dir(xhr);
+}
