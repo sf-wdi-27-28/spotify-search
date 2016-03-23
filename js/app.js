9b8d4284898b7e46ee026bb8618797c77ae8935d
srhbnnstr <srhbnnstr@gmail.com>
2016-03-22 20:50:07 -0700

    js update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
+var spotifyApi = "https://api.spotify.com/v1/search";
+
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  // your code here
+  getSearchResults();
 
+$("input").on("submit", function(e) {
+  e.preventDefault();
+
+  getSearchResults();
 });
+});
+
+
+  // your code here
+  function getSearchResults() {
+  $.ajax({
+    method: "GET",
+    url: spotifyApi,
+    data: $("form").serialize(),
+    success: onSuccess,
+    error: onError
+  });
+}
