13808677ca1800a198eb0db725c2c9889c8536b0
mbd-s <michaelbsilverberg@gmail.com>
2016-03-22 19:46:44 -0700

    built Spotify search that doesn't yet refresh

--- js/app.js
+++ js/app.js
@@ -1,9 +1,40 @@
+var spotify_api = "https://api.spotify.com/v1/search";
+
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  // your code here
+  $("form").on("submit", function(event){
+    event.preventDefault();
+    searchTrack();
+  });
+
+function searchTrack(){
+  $.ajax({
+    method: "GET",
+    url: spotify_api,
+    data: $("form").serialize(),
+      success: onSuccess,
+      error: onError
+  });
+}
+
+function onSuccess(json){
+  console.log(json);
+  var trackResults = json.tracks.items;
+  trackResults.forEach(function(element) {
+   $("#results").append('<ul>"' + element.name + '" - ' + element.artists[0].name + '</>');
+   //tracks.items (has inside array)
+});
+}
+
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+}
 
 });
