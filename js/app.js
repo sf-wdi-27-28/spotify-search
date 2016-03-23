42a391650e0ef2ced4491efc74cda6acea91340e
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-22 19:34:14 -0700

    one small step closer

--- js/app.js
+++ js/app.js
@@ -5,26 +5,28 @@ $(document).on('ready', function() {
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  $(".form-control").on("click", function findSong(e){
-    console.log(this);
+  $("#spotify-track").on("click", function findSong(e){
+    console.log(e);
     $.ajax({
       method:'GET',
-      url: "https://api.spotify.com/v1/search",
+      url:"https://api.spotify.com/v1/search",
       data: $(this).serialize(),
       success: onSuccess,
       error: onError
     });
   });
 
-function onSuccess(element){
-console.log(element);
-}
+function onSuccess(json){
+json.tracks.forEach( function findTracks(element){
+   $('#results').append(element);
+    console.log(json);
+  });
 
-function onError(xhr, status, errorThrown) {
-    alert("Sorry, there was a problem!");
-    console.log("Error: " + errorThrown);
-    console.log("Status: " + status);
-    console.log(xhr);
+  function onError (xhr, status, errorThrown){
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.log(xhr);
+    }
   }
-
 });
