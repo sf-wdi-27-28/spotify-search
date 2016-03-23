c4dbcff8eb3c01b166145f1ec2ad710a0bac4693
PJC-1 <jumpzine@gmail.com>
2016-03-22 22:26:22 -0700

    save again

--- js/app.js
+++ js/app.js
@@ -5,5 +5,29 @@ $(document).on('ready', function() {
   console.log('JS is loaded!');
 
   // your code here
+  var spotApi = "https://api.spotify.com/v1/search?type=track&q=";
 
+  $(".search").on("submit", function listen(event) {
+    var data = $("#value").val();
+    event.preventDefault();
+    console.log(data);
+    $.ajax({
+      method: 'GET',
+      url: spotApi + data,
+      success: onSuccess,
+      error: onError
+      });
+    });
 });
+
+function onSuccess(json){
+  console.log(json);
+  var path = json.tracks.items;
+  path.forEach(function (element){
+    $("#results").append("<li>" + element.artists[0].name + "-" + element.name +"</li>");
+  });
+}
+
+function onError(xhr, status, errorThrown){
+  console.log('Error',json, status, errorThrown);
+}
