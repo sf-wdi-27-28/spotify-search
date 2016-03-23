ed4d779806349dff56689a9a0dbd40cd047cbd42
ilterates <iltates@gmail.com>
2016-03-22 19:15:53 -0700

    working on getting correct url

--- js/app.js
+++ js/app.js
@@ -3,7 +3,29 @@ $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
+var spoUrl= "https://api.spotify.com/v1/search";
 
-  // your code here
 
+  $.ajax(
+              {
+    method: 'GET',
+       url: spoUrl,
+    artists: {
+          q:"artistName",
+       type:"track",
+            },
+    dataType: "json",
+    success: onSuccess
+              }
+
+);
 });
+$(".Search").on("submit", function () {
+  event.preventDefault();
+  $("#result").append("<ul>"+"<il>" + json.artists.items  +"</li>"+"</ul>");
+
+  console.log(json.artists);
+});
+function onSuccess() {
+console.log();
+}
