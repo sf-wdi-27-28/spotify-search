d79538e7d094c2e52ac4d930c75d10429368849e
seanvsville <seanvanho@gmail.com>
2016-03-22 19:46:20 -0700

    finished setting up qquery to get artist name and song

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    $("#track").empty();
+    callTrack();
+  });
 
-  // your code here
+  function callTrack() {
+    $.ajax({
+      type: 'GET',
+      url: "https://api.spotify.com/v1/search",
+      data: $("form").serialize(),
+      success: onSuccess,
+      error: onError,
+    });
+
+    function onSuccess(data) {
+      console.log(data.tracks.items);
+      data.tracks.items.forEach(function(element) {
+        $("#tracks").append("<li>" + element.artists[0].name + " - " + element.name+"</li>");
+      });
+    }
+
+    function onError(xhr, status, errorThrown) {
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+    }
+  }
 
 });
