852d247ebe0707a8146a6ed0bca426afaedd0250
Herman Chan <hermchan89@gmail.com>
2016-03-23 00:47:12 -0700

    edited js. not working correctly

--- js/app.js
+++ js/app.js
@@ -1,33 +1,42 @@
-var searchTracks = "https://api.spotify.com/v1/search";
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
+  var searchUrl = "https://api.spotify.com/v1/search";
+
+  $("form").on("submit", function (event){
+    event.preventDefault();
+    $("#result").empty();
+    input();
+  });
   // check to make sure JS is loaded
   console.log('JS is loaded!');
-  $.ajax({
-    method: "GET",
-    url: searchTracks,
-    data: {
-      api_key: '6rqhFgbbKwnb9MLmUQDhG6',
-      q: 'drake',
-      type: 'track',
-    },
-    success: onSuccess,
-    error: onError
-  });
+  function input(){
+    $.ajax({
+      method: "GET",
+      url: searchUrl,
+      data: {
+        api_key: '6rqhFgbbKwnb9MLmUQDhG6',
+        q: $("#song").val(),
+        type: 'track',
+      },
+      success: onSuccess,
+      error: onError
+    });
+  }
   // your code here
-  $('.btn').on('click', function(event){});
-});
-
-function onSuccess(json){
+  // trying to print out artist name and track.
+  // it was working but something happened and I don't know how to fix it
+function onSuccess(json) {
   for (var i=0;i<json.tracks.items[0].artists.length;i++){
     console.log(json.tracks.items[0].artists[i].name);
   }
   console.log("Track name:", json.tracks.items[0].name);
-}
+  }
 
-function onError(xhr, status, errorThrown){
-  console.log("Error: " + errorThrown);
-  console.log("Status: " + status);
-  console.log(xhr);
-}
+    function onError(xhr, status, errorThrown){
+      alert("Sorry, please try again!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.log(xhr);
+    }
+  });
