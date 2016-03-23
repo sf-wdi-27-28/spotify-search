e5833c043bb4cb3339c82f3109081d6340203f39
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-22 18:20:37 -0700

    added input and starter code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
+var spotify_api = "https://api.spotify.com/v1/search";
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
+  getTracks();
+  //
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+  }
+
   // your code here
+  function getTracks(){
+    $.ajax({
+      method: "GET",
+      url: spotify_api,
+      data: $("form").serialize(),
+      success: onSuccess,
+      error: onError
+    });
+  }
+
+  function onSuccess(json){
+      console.log(json);
+    }
+
+  function onError(){
+    alert("You fucked up!");
+  }
+
+
+  // function onSuccess(json){
+  //
+  // }
 
 });
