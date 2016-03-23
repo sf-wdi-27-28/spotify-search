25a7fea819e29c9e726326b21c937f5a3e44f057
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-22 18:54:23 -0700

    feeling a little stuck

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
+
 // wait for DOM to load before running JS
 $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
 
-  // your code here
+  $(".form-control").on("click", function findSong(e){
+    console.log(this);
+    $.ajax({
+      method:'GET',
+      url: "https://api.spotify.com/v1/search",
+      data: $(this).serialize(),
+      success: onSuccess,
+      error: onError
+    });
+  });
+
+function onSuccess(element){
+console.log(element);
+}
+
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.log(xhr);
+  }
 
 });
